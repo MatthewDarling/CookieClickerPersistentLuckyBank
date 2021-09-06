7e46efaefc4ba95303d00a956b4f5bd223575d10
Matthew Darling <matthewjdarling@gmail.com>
2021-09-06 19:18:04 -0400

    New strategy that supports buffs
    It's actually pretty complicated to summarize all the effects that turn `Game.unbuffedCps` into `Game.cookiesPs`. Dividing one by the other works, but it didn't seem elegant to take the two numbers, divide, calculate the `minLuckyAmount` with and without buffs, then apply the multiplier.
    
    But `calcLuckyAmount` has two inputs, and I tested it by varying each parameter separately. So I realized today (after having a fizzled Conjure Baked Goods reduce CPS) that if I wanted to account for buffs, I could switch the banking logic to use the inputs to `calcLuckyAmount` instead of its output.
    
    Now there's less code and it has more features :)

--- main.js
+++ main.js
@@ -1,13 +1,8 @@
-// If I want to make the minLuckyAmount work properly with buffs, here's how the game calculates buffed CPS:
-//
-// for (var i in Game.buffs)
-// {
-//     if (typeof Game.buffs[i].multCpS!=='undefined') mult*=Game.buffs[i].multCpS;
-// }
+// Eventually, should also support Conjure Baked Goods from the Wizard Tower Grimoire
 
 myModObject = {
-    resetMinLuckyAmount: function() {
-        Game.minLuckyAmount = 0;
+    resetMinCookieBank: function() {
+        Game.minCookieBank = 0;
     },
     // These helper function will be added to the Golden Cookie definition, by this.init()
     // These injections aren't strictly necessary, because this original object can be referenced through Game.mods
@@ -22,18 +17,12 @@ myModObject = {
                              this.fifteenMinOfProduction(cps));
     },
     moddedLuckyAmount: function(goldenCookieMult) {
-        var moniWithBuffs = goldenCookieMult * this.calcLuckyAmount(Game.cookies, Game.cookiesPs);
+        // If the current cookie total is higher than the "cookie bank", save as the new cookie bank
+        Game.minCookieBank = Math.max(Game.minCookieBank, Game.cookies)
 
-        // This will become the Lucky! floor, using unbuffedCps
-        // Properly accounting for buffs would mean tracking the min amount for each unique buff combo
-        // And I don't want to deal with that yet
-        var moniWithoutBuffs = goldenCookieMult * this.calcLuckyAmount(Game.cookies, Game.unbuffedCps);
-
-        // If bigger than existing Lucky! floor, overwrite with new value
-        Game.minLuckyAmount = Math.max(Game.minLuckyAmount, moniWithoutBuffs);
-
-        // Lastly, implement the Lucky! floor by returning that value if larger than the original formula
-        return Math.max(moniWithBuffs, Game.minLuckyAmount);
+        // Run the original Lucky! formula, changing just the cookie input value
+        // This accounts properly for all the many ways cookies-per-second can be modified
+        return goldenCookieMult * this.calcLuckyAmount(Game.minCookieBank, Game.cookiesPs);
     },
     // End of helper methods
     
@@ -283,8 +272,8 @@ myModObject = {
         me.die();
     },
     init: function() {
-        if (typeof Game.minLuckyAmount === 'undefined') { Game.minLuckyAmount = 0; }
-        Game.registerHook('reset', this.resetMinLuckyAmount);
+        if (typeof Game.minCookieBank === 'undefined') { Game.minCookieBank = 0; }
+        Game.registerHook('reset', this.resetMinCookieBank);
 
         // Inject functions into the Golden Cookie object
         // Strictly speaking, only the popFunc inject is necessary, but I got tired of pulling helper functions from Game.mods
@@ -296,12 +285,12 @@ myModObject = {
         Game.shimmerTypes['golden'].popFunc = this.newGoldenCookiePop;
     },
     save: function() {
-        return Game.minLuckyAmount.toString();
+        return Game.minCookieBank.toString();
     },
     load: function(dataStr) {
         const parsed = parseInt(dataStr);
         if (!isNaN(parsed)) {
-            Game.minLuckyAmount = parsed;
+            Game.minCookieBank = parsed;
         }
     }
 };
